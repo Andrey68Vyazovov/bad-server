bf8212d4b26d426cca99faf8c2e03ba930215663
Andrey68Vyazovov <139168951+Andrey68Vyazovov@users.noreply.github.com>
2025-03-16 14:43:33 +0300

    fix: fix controllers #2

--- backend/src/routes/customers.ts
+++ backend/src/routes/customers.ts
@@ -6,25 +6,26 @@ import {
     updateCustomer,
 } from '../controllers/customers'
 import auth, { roleGuardMiddleware } from '../middlewares/auth'
+import { doubleCsrfProtection } from '../middlewares/csrf-protect' // Добавляем CSRF-защиту
 import { Role } from '../models/user'
 
 const customerRouter = Router()
 
+// Маршруты для получения данных
 customerRouter.get('/', auth, roleGuardMiddleware(Role.Admin), getCustomers)
-customerRouter.get(
-    '/:id',
-    auth,
-    roleGuardMiddleware(Role.Admin),
-    getCustomerById
-)
+customerRouter.get('/:id', auth, roleGuardMiddleware(Role.Admin), getCustomerById)
+
+// Маршруты для изменения данных (с CSRF-защитой)
 customerRouter.patch(
     '/:id',
+    doubleCsrfProtection, // Добавляем CSRF-защиту
     auth,
     roleGuardMiddleware(Role.Admin),
     updateCustomer
 )
 customerRouter.delete(
     '/:id',
+    doubleCsrfProtection, // Добавляем CSRF-защиту
     auth,
     roleGuardMiddleware(Role.Admin),
     deleteCustomer
